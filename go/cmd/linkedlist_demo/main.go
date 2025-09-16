60b275a7ec34199129ae9fb1ad783d1f582b35bc
Eli Headley <eliheadley@gmail.com>
2025-09-15 20:45:09 -0400

    Add an insert after function

--- go/cmd/linkedlist_demo/main.go
+++ go/cmd/linkedlist_demo/main.go
@@ -3,19 +3,24 @@ package main
 import (
 	"fmt"
 
-	"datastructures/linkedlist"
+	ll "cs-concepts/datastructures/linkedlist"
 )
 
 func main() {
 	fmt.Println("Welcome to the Go Linked List Example")
-	list := linkedlist.LinkedList[int]{}
-	list.Print()
+	list := ll.LinkedList[int]{}
 
 	list.Append(1)
 	list.Append(2)
 	list.Append(3)
 	list.Append(4)
 	list.Append(5)
+
+	list.Print()
+	err := list.InsertAfter(5, 6)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
 
 	list.Print()
 }
--- go/datastructures/linkedlist/linkedlist.go
+++ go/datastructures/linkedlist/linkedlist.go
@@ -1,8 +1,13 @@
 package linkedlist
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Append, Prepend, InsertAfter(node, value), Delete(value), Find(value).
+
+var IndexOutOfBounds = errors.New("Index is out of bounds for the current size of the list")
 
 type Node[T any] struct {
 	next *Node[T]
@@ -12,31 +17,77 @@ type Node[T any] struct {
 
 type LinkedList[T any] struct {
 	head   *Node[T]
+	tail   *Node[T]
 	length int
 }
 
 func (list *LinkedList[T]) Append(newVal T) {
 	newNode := &Node[T]{nil, nil, newVal}
-	if list.head == nil {
+	if list.head == nil && list.tail == nil {
 		list.head = newNode
+		list.tail = newNode
 		list.length++
 		return
 	}
 
+	list.tail.next = newNode
+	newNode.prev = list.tail
+	list.tail = newNode
+	list.length++
+}
+
+func (list *LinkedList[T]) Prepend(newVal T) {
+	newNode := &Node[T]{nil, nil, newVal}
+	if list.head == nil && list.tail == nil {
+		list.head = newNode
+		list.tail = newNode
+		list.length++
+		return
+	}
+
+	list.head.prev = newNode
+	newNode.next = list.head
+	list.head = newNode
+	list.length++
+}
+
+func (list *LinkedList[T]) InsertAfter(index int, newVal T) error {
+	if index < 0 || index >= list.length {
+		return IndexOutOfBounds
+	}
+
+	// Inserting after the last node → append
+	if index == list.length-1 {
+		list.Append(newVal)
+		return nil
+	}
+
+	// Find the node at position index
 	cur := list.head
-	for cur.next != nil {
+	for i := 0; i < index; i++ {
 		cur = cur.next
 	}
+
+	next := cur.next
+	newNode := &Node[T]{val: newVal}
+
+	newNode.prev = cur
+	newNode.next = next
 	cur.next = newNode
+	if next != nil {
+		next.prev = newNode
+	}
+
 	list.length++
+	return nil
 }
 
 func (list LinkedList[T]) Print() {
 	fmt.Print("head->")
 	cur := list.head
 	for cur != nil {
-		fmt.Printf("[%v]->", cur.val)
+		fmt.Printf("[%v]<-->", cur.val)
 		cur = cur.next
 	}
-	fmt.Println("end")
+	fmt.Println("<-tail")
 }
